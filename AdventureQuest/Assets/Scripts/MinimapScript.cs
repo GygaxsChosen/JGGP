b15d63918e6bbd28b67edd9801baf0ff688d6531
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-07-02 00:07:22 -0400

    Working, uncentered minimap
    -created minimap

---
+++ AdventureQuest/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour {
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector2 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        newPosition.x = transform.position.x;
+        transform.position = newPosition;
+    }
+
+}
