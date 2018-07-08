a086901037e5f93529f2531490b76938dba82dbc
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-07-08 11:47:06 -0400

    World map v1 (#4)
    * Got basic movement working
    
    -added a 2D map (primitive)
    -imported a character
    -added movement (4 directions for now,  not 8)
    -added basic scripts for camera and player
    
    * Working, uncentered minimap
    
    -created minimap
    
    * Fixed my oops
    
    -fixed camera not being centered on player
    
    * Updated scene
    
    * Got basic randomization working
    
    -Got basic randomization working (unconstrained)
    -Almost ready for PR once constrained
    
    * Added layers, fixed animation(s)

---
+++ AdventureQuest/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject[] tiles;
+    public GameObject wall;
+    public List <Vector3> createdTiles;
+
+    public int gridSize;
+    public float tileSize;
+
+    public float chanceGrass;
+    public float chanceFlowers;
+    //public float chanceRock;
+
+    public float waitTime;
+
+	void Start () {
+        StartCoroutine(GenerateLevel());
+	}
+
+    IEnumerator GenerateLevel()
+    {
+        for (int i=0; i < gridSize; i++)
+        {
+            for (int j=0; j < gridSize; j++)
+            {
+                transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+                float typeOfTile = Random.Range(0f, 1f);
+                CallCreateTile(typeOfTile);
+            
+                //int tile = Random.Range(0, tiles.Length);
+                //if (tile != 0 && tile != 1 && tile != 2)
+                //{
+                //    tile = Random.Range(0, tiles.Length);
+                //}
+
+                //CreateTile(tile);
+
+                yield return new WaitForSeconds(waitTime);
+            }
+            
+        }
+        yield return 0;
+    }
+
+    void CallCreateTile(float type)
+    {
+        if (type < chanceGrass)
+        {
+            int variation = 0;
+            CreateTile(variation);
+        } else if (type < chanceFlowers)
+        {
+            int variation = Random.Range(1, 3);
+            CreateTile(variation);
+        } else
+        {
+            int variation = Random.Range(3, 6);
+            CreateTile(variation);
+        }
+    }
+
+    //void TileSelector(int type)
+    //{
+    //    switch (type)
+    //    {
+    //        case 0:
+    //            transform.position = new Vector3(transform.position.x, transform.position.y + tileSize, 0);
+    //            break;
+    //        case 1:
+    //            transform.position = new Vector3(transform.position.x + tileSize, transform.position.y, 0);
+    //            break;
+    //        case 2:
+    //            transform.position = new Vector3(transform.position.x, transform.position.y - tileSize, 0);
+    //            break;
+    //        case 3:
+    //            transform.position = new Vector3(transform.position.x - tileSize, transform.position.y, 0);
+    //            break;
+    //    }
+    //}
+	
+    void CreateTile(int tileIndex)
+    {
+        GameObject tileObject;
+        tileObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
+
+        createdTiles.Add(tileObject.transform.position);
+    }
+}
---
+++ AdventureQuest/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+}
