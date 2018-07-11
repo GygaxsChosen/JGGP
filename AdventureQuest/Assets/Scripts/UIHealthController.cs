91d57f786c2ab57fd21b859013c73b533182db82
STICH25 <ahernandez2501@gmail.com>
2018-07-10 22:02:43 -0400

    This is the UI controller

---
+++ AdventureQuest/Assets/Scripts/UIHealthController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UIHealthController : MonoBehaviour 
+{
+    public bool m_UseRelativeLocation = true;
+
+    private Quaternion m_RelativeLocation;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_RelativeLocation = transform.parent.localRotation;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (m_UseRelativeLocation)
+            transform.rotation = m_RelativeLocation;
+	}
+}
