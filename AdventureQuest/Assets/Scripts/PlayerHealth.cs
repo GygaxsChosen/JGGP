c332760a974ff6a449061dc10ac764f3bf90f089
STICH25 <ahernandez2501@gmail.com>
2018-07-10 22:01:30 -0400

    Test commit from visual studio

---
+++ AdventureQuest/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float p_StartingHealth = 100f;
+    public Slider p_Slider;
+    public Image p_FillImage;
+    public Color p_FullHealthColor = Color.red;
+    public Color p_ZeroHealthColor = Color.white;
+
+    private float p_CurrrentHealth;
+    private bool p_Dead;
+
+
+    // Use this for initialization
+    private void OnEnable()
+    {
+        p_CurrrentHealth = p_StartingHealth;
+        p_Dead = false;
+
+        SetHealthUI();
+
+
+    }
+
+    public void TakeDamage(float amount)
+    {
+        //amount of danage taken by enemies, will varie on enemy type
+
+        p_CurrrentHealth -= amount;
+
+        SetHealthUI();
+
+        if (p_CurrrentHealth <= 0f && !p_Dead)
+        {
+            OnDeath();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    private void SetHealthUI()
+    {
+
+        p_Slider.value = p_CurrrentHealth;
+
+        p_FillImage.color = Color.Lerp(p_ZeroHealthColor, p_FullHealthColor, p_CurrrentHealth / p_StartingHealth);
+    }
+
+    private void OnDeath()
+    {
+        p_Dead = true;
+
+        gameObject.SetActive(true);
+
+    }
+}
