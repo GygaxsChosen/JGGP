b868688318ebfd2263a410e95f9b94ae0aff2371
STICH25 <ahernandez2501@gmail.com>
2018-07-15 15:56:02 -0400

    New Scripts and managers
    Added the srcripts for the enenemy and the managersa the loaded script is also there but we need to talk about that

---
+++ AdventureQuest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+}
