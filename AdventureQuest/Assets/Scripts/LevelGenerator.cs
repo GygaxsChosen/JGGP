5953239ed06f0ec752b39f53e8f4fe04263fc433
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-07-17 00:03:42 -0400

    Added houses, added walls
    -still working on collisions

--- AdventureQuest/Assets/Scripts/LevelGenerator.cs
+++ AdventureQuest/Assets/Scripts/LevelGenerator.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour {
 
     public GameObject[] tiles;
-    public GameObject wall;
+    public GameObject[] walls;
     public List <Vector3> createdTiles;
 
     public int gridSize;
@@ -13,7 +13,6 @@ public class LevelGenerator : MonoBehaviour {
 
     public float chanceGrass;
     public float chanceFlowers;
-    //public float chanceRock;
 
     public float waitTime;
 
@@ -30,19 +29,41 @@ public class LevelGenerator : MonoBehaviour {
                 transform.position = new Vector3(tileSize * i, tileSize * j, 0);
                 float typeOfTile = Random.Range(0f, 1f);
                 CallCreateTile(typeOfTile);
-            
-                //int tile = Random.Range(0, tiles.Length);
-                //if (tile != 0 && tile != 1 && tile != 2)
-                //{
-                //    tile = Random.Range(0, tiles.Length);
-                //}
 
-                //CreateTile(tile);
-
-                yield return new WaitForSeconds(waitTime);
+                //only necessary if we want to see individual tiles generated
+                //yield return new WaitForSeconds(waitTime);
             }
             
         }
+
+        transform.position = new Vector3(0, 0, 0);
+        for (int i = 0; i < gridSize; i++)
+        {
+            transform.position = new Vector3((tileSize * i), -.15f, 0);
+            CreateWall(0);
+        }
+
+        transform.position = new Vector3(0, gridSize * tileSize, 0);
+        for (int i = 0; i < gridSize; i++)
+        {
+            transform.position = new Vector3((tileSize * i), gridSize * tileSize - .15f, 0);
+            CreateWall(0);
+        }
+
+        transform.position = new Vector3(0, 0, 0);
+        for (int j = 0; j < gridSize; j++)
+        {
+            transform.position = new Vector3(-.08f, j * tileSize -0.065f, 0);
+            CreateWall(1);
+        }
+
+        transform.position = new Vector3(gridSize * tileSize, 0, 0);
+        for (int j = 0; j < gridSize; j++)
+        {
+            transform.position = new Vector3(tileSize * gridSize, j * tileSize - 0.065f, 0);
+            CreateWall(1);
+        }
+
         yield return 0;
     }
 
@@ -62,25 +83,6 @@ public class LevelGenerator : MonoBehaviour {
             CreateTile(variation);
         }
     }
-
-    //void TileSelector(int type)
-    //{
-    //    switch (type)
-    //    {
-    //        case 0:
-    //            transform.position = new Vector3(transform.position.x, transform.position.y + tileSize, 0);
-    //            break;
-    //        case 1:
-    //            transform.position = new Vector3(transform.position.x + tileSize, transform.position.y, 0);
-    //            break;
-    //        case 2:
-    //            transform.position = new Vector3(transform.position.x, transform.position.y - tileSize, 0);
-    //            break;
-    //        case 3:
-    //            transform.position = new Vector3(transform.position.x - tileSize, transform.position.y, 0);
-    //            break;
-    //    }
-    //}
 	
     void CreateTile(int tileIndex)
     {
@@ -88,5 +90,13 @@ public class LevelGenerator : MonoBehaviour {
         tileObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
 
         createdTiles.Add(tileObject.transform.position);
+    }
+
+    void CreateWall(int wallIndex)
+    {
+        GameObject wallObject;
+        wallObject = Instantiate(walls[wallIndex], transform.position, transform.rotation) as GameObject;
+
+        createdTiles.Add(wallObject.transform.position);
     }
 }
