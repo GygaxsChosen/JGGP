d8991d416b80a0d88b6c43039719237ae2827cdf
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-07-22 23:00:31 -0400

    First run at collisions
    -added initial collisions with walls, buildings, environment. needs to be tweaked.
    -deleted unused animations and make our game objects prefabs
    -added an example of scene transitions
    -added tags to enhance collisions. may change.

---
+++ AdventureQuest/Assets/Scripts/SceneLoaderAtoB.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderAtoB : MonoBehaviour {
+
+    private float Timer = 0.0f;
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    // Use this for initialization
+    void Start () {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+        //scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+
+        if (Timer > 3)
+        {
+            SceneManager.LoadScene("Enemy with new Manages");
+
+        }
+        else if (Timer > 6)
+        {
+            Timer = 0;
+            SceneManager.LoadScene("July16_Collisions");
+        }
+    }
+}
