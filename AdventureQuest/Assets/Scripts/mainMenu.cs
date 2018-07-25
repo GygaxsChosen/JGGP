a34dac80abc13ba077d98c9aa387571b31ee3a7e
Scottie Thurwanger <thurwangers@gmail.com>
2018-07-25 11:46:30 -0400

    More enhancements, deleted old files

--- AdventureQuest/Assets/Scripts/mainMenu.cs
+++ AdventureQuest/Assets/Scripts/mainMenu.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour {
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
 
-public void playGame()
+
+    public void playGame()
     {
-        SceneManager.LoadScene("July16_Collisions");
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+        SceneManager.LoadScene("Level1");
     }
 }
