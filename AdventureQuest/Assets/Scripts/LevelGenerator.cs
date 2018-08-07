0c35536e16636b955a4a7ccb6169e49ef08b23d6
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-08-07 18:23:37 -0400

    checkin -- saving changes

--- AdventureQuest/Assets/Scripts/LevelGenerator.cs
+++ AdventureQuest/Assets/Scripts/LevelGenerator.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour {
 
+    #region Public Properties
     public GameObject[] tiles;
     public GameObject[] walls;
-    public List <Vector3> createdTiles;
-
-    public int gridSize;
+    public List <Vector3> createdGrass;
+    public List<Vector3> createdRockGrass;
+    public List<Vector3> createdTrees;
+    public List<Vector3> createdWalls;
     public float tileSize;
-
     public float chanceGrass;
     public float chanceFlowers;
-
+    public float chanceTrees;
     public float waitTime;
+    #endregion
+
+    #region Public Properties
+    private int Width = 30;
+    private int Height = 30;
+
+
+
+
 
 	void Start () {
         StartCoroutine(GenerateLevel());
@@ -22,74 +32,128 @@ public class LevelGenerator : MonoBehaviour {
 
     IEnumerator GenerateLevel()
     {
-        for (int i=0; i < gridSize; i++)
+        for (int j=0; j < 10; j++)
         {
-            for (int j=0; j < gridSize; j++)
+            for (int i = 0; i < Width; i++)
             {
                 transform.position = new Vector3(tileSize * i, tileSize * j, 0);
                 float typeOfTile = Random.Range(0f, 1f);
-                CallCreateTile(typeOfTile);
-
-                //only necessary if we want to see individual tiles generated
-                //yield return new WaitForSeconds(waitTime);
+                CallCreateGrass(typeOfTile);
             }
-            
+        }
+        for (int j = 10; j < Height; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+                float typeOfTile = Random.Range(0f, 1f);
+                CallCreateGrassWithRocks(typeOfTile);
+            }
+        }
+        for (int j = 0; j < 10; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+                float typeOfTile = Random.Range(0f, 1f);
+                CallCreateTrees(typeOfTile);
+            }
         }
 
         transform.position = new Vector3(0, 0, 0);
-        for (int i = 0; i < gridSize; i++)
+        for (int i = 0; i < Width; i++)
         {
             transform.position = new Vector3((tileSize * i), -.15f, 0);
             CreateWall(0);
         }
 
-        transform.position = new Vector3(0, gridSize * tileSize, 0);
-        for (int i = 0; i < gridSize; i++)
+        transform.position = new Vector3(0, Width * tileSize, 0);
+        for (int i = 0; i < Width; i++)
         {
-            transform.position = new Vector3((tileSize * i), gridSize * tileSize - .15f, 0);
+            transform.position = new Vector3((tileSize * i), Width * tileSize - .15f, 0);
             CreateWall(0);
         }
 
         transform.position = new Vector3(0, 0, 0);
-        for (int j = 0; j < gridSize; j++)
+        for (int j = 0; j < Height; j++)
         {
-            transform.position = new Vector3(-.08f, j * tileSize -0.065f, 0);
+            transform.position = new Vector3(-.08f, j * tileSize - 0.065f, 0);
             CreateWall(1);
         }
 
-        transform.position = new Vector3(gridSize * tileSize, 0, 0);
-        for (int j = 0; j < gridSize; j++)
+        transform.position = new Vector3(Height * tileSize, 0, 0);
+        for (int j = 0; j < Height; j++)
         {
-            transform.position = new Vector3(tileSize * gridSize -.05f, j * tileSize - 0.065f, 0);
+            transform.position = new Vector3(tileSize * Height - .05f, j * tileSize - 0.065f, 0);
             CreateWall(1);
         }
 
         yield return 0;
     }
 
-    void CallCreateTile(float type)
+    void CallCreateGrass(float type)
     {
         if (type < chanceGrass)
         {
             int variation = 0;
-            CreateTile(variation);
-        } else if (type < chanceFlowers)
+            CreateGrass(variation);
+        }
+        else
         {
             int variation = Random.Range(1, 3);
-            CreateTile(variation);
-        } else
-        {
-            int variation = Random.Range(3, 6);
-            CreateTile(variation);
+            CreateGrass(variation);
         }
     }
-	
-    void CreateTile(int tileIndex)
-    {
-        GameObject tileObject;
-        tileObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
 
-        createdTiles.Add(tileObject.transform.position);
+    void CallCreateGrassWithRocks(float type)
+    {
+        if (type < chanceGrass)
+        {
+            int variation = 0;
+            CreateGrassWithRocks(variation);
+        }
+        else if (type < chanceFlowers)
+        {
+            int variation = Random.Range(1, 3);
+            CreateGrassWithRocks(variation);
+        }
+        else
+        {
+            int variation = Random.Range(3, 6);
+            CreateGrassWithRocks(variation);
+        }
+    }
+
+    void CallCreateTrees(float type)
+    {
+        if (type < chanceTrees)
+        {
+            CreateTrees(6);
+        }
+    }
+
+    void CreateGrass(int tileIndex)
+    {
+        GameObject grass;
+        grass = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
+
+        createdGrass.Add(grass.transform.position);
+    }
+
+    void CreateGrassWithRocks(int tileIndex)
+    {
+        GameObject rockGrass;
+        rockGrass = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
+
+        createdRockGrass.Add(rockGrass.transform.position);
+    }
+
+    void CreateTrees(int tileIndex)
+    {
+        GameObject treeObject;
+        treeObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
+
+        createdTrees.Add(treeObject.transform.position);
     }
 
     void CreateWall(int wallIndex)
@@ -97,6 +161,6 @@ public class LevelGenerator : MonoBehaviour {
         GameObject wallObject;
         wallObject = Instantiate(walls[wallIndex], transform.position, transform.rotation) as GameObject;
 
-        createdTiles.Add(wallObject.transform.position);
+        createdWalls.Add(wallObject.transform.position);
     }
 }
